76ba63fbe8b258ecac6477cac9ca20d6d2626d9e
daniel <daniel@besidemagazine.com>
2022-05-14 15:42:56 -0300

    number or point

--- currencies.js
+++ currencies.js
@@ -3,102 +3,103 @@ const fs = require("fs");
 const getCurrencies = (date) => {
     const data = fs.readFileSync(`./files/${date}.txt`, "utf8");
     const rows = data.split("\n");
-    rows.pop()
-    const currecies = []
+    rows.pop();
+    const currecies = [];
     for (const row of rows) {
-        const c = parseCurrency(row)
-        currecies.push(c)
+        const c = parseCurrency(row);
+        currecies.push(c);
     }
-    return currecies
-}
+    return currecies;
+};
 
 const parseCurrency = (row) => {
     const currency = {
         name: "CAD USD",
-        openInterest: 0,
-        totalChange: 0,
-        totalTraders: 0,
+        openInterest: NaN,
+        totalChange: NaN,
+        totalTraders: NaN,
         contracts: "",
         metrics: [
             {
                 name: "Dealer Intermediary",
                 long: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 short: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 spreading: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 }
             },
             {
                 name: "Asset Manager/Institutional",
                 long: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 short: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 spreading: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 }
             },
             {
                 name: "Leveraged Funds",
                 long: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 short: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 },
                 spreading: {
-                    positions: 0,
-                    openInt: 0,
-                    traders: 0,
-                    changes: 0,
+                    positions: NaN,
+                    openInt: NaN,
+                    traders: NaN,
+                    changes: NaN,
                 }
             },
         ]
-    
+
     };
 
     const columns = row.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
     const header = ['name', 'numberDate', 'date', 'CFTCCode', 'noClue1', 'noClue2', 'noClue3', 'openInterest'];
     currency.name = columns[0].trim();
-    currency.metrics[0].long.positions = Number.parseFloat(columns[8].trim());
-    currency.metrics[0].short.positions = Number.parseFloat(columns[9].trim());
-    currency.metrics[0].spreading.positions = Number.parseFloat(columns[10].trim());
-    currency.metrics[1].long.positions = Number.parseFloat(columns[11].trim());
-    currency.metrics[1].short.positions = Number.parseFloat(columns[12].trim());
-    currency.metrics[1].spreading.positions = Number.parseFloat(columns[13].trim());
-    currency.metrics[2].long.positions = Number.parseFloat(columns[14].trim());
-    currency.metrics[2].short.positions = Number.parseFloat(columns[15].trim());
-    currency.metrics[2].spreading.positions = Number.parseFloat(columns[16].trim());
+    currency.openInterest = cleanNumber(columns[7]);
+    currency.metrics[0].long.positions = cleanNumber(columns[8]);
+    currency.metrics[0].short.positions = cleanNumber(columns[9]);
+    currency.metrics[0].spreading.positions = cleanNumber(columns[10]);
+    currency.metrics[1].long.positions = cleanNumber(columns[11]);
+    currency.metrics[1].short.positions = cleanNumber(columns[12]);
+    currency.metrics[1].spreading.positions = cleanNumber(columns[13]);
+    currency.metrics[2].long.positions = cleanNumber(columns[14]);
+    currency.metrics[2].short.positions = cleanNumber(columns[15]);
+    currency.metrics[2].spreading.positions = cleanNumber(columns[16]);
     /* Other reportables
     currency.metrics[3].short.changes = Number.parseFloat(columns[17].trim());
     currency.metrics[3].short.changes = Number.parseFloat(columns[18].trim());
@@ -112,16 +113,16 @@ const parseCurrency = (row) => {
      currency.metrics[3].short.changes = Number.parseFloat(columns[22].trim());
      currency.metrics[3].short.changes = Number.parseFloat(columns[23].trim());
      */
-    currency.totalChange = Number.parseFloat(columns[24].trim());
-    currency.metrics[0].long.changes = Number.parseFloat(columns[25].trim());
-    currency.metrics[0].short.changes = Number.parseFloat(columns[26].trim());
-    currency.metrics[0].spreading.changes = Number.parseFloat(columns[27].trim());
-    currency.metrics[1].long.changes = Number.parseFloat(columns[28].trim());
-    currency.metrics[1].short.changes = Number.parseFloat(columns[29].trim());
-    currency.metrics[1].spreading.changes = Number.parseFloat(columns[30].trim());
-    currency.metrics[2].long.changes = Number.parseFloat(columns[31].trim());
-    currency.metrics[2].short.changes = Number.parseFloat(columns[32].trim());
-    currency.metrics[2].spreading.changes = Number.parseFloat(columns[33].trim());
+    currency.totalChange = cleanNumber(columns[24]);
+    currency.metrics[0].long.changes = cleanNumber(columns[25]);
+    currency.metrics[0].short.changes = cleanNumber(columns[26]);
+    currency.metrics[0].spreading.changes = cleanNumber(columns[27]);
+    currency.metrics[1].long.changes = cleanNumber(columns[28]);
+    currency.metrics[1].short.changes = cleanNumber(columns[29]);
+    currency.metrics[1].spreading.changes = cleanNumber(columns[30]);
+    currency.metrics[2].long.changes = cleanNumber(columns[31]);
+    currency.metrics[2].short.changes = cleanNumber(columns[32]);
+    currency.metrics[2].spreading.changes = cleanNumber(columns[33]);
     /* Other reportables
     currency.metrics[3].short.changes = Number.parseFloat(columns[34].trim());
     currency.metrics[3].short.changes = Number.parseFloat(columns[35].trim());
@@ -138,15 +139,15 @@ const parseCurrency = (row) => {
     /* No clue 
      currency.something = Number.parseFloat(columns[41].trim());
      */
-    currency.metrics[0].long.openInt = Number.parseFloat(columns[42].trim());
-    currency.metrics[0].short.openInt = Number.parseFloat(columns[43].trim());
-    currency.metrics[0].spreading.openInt = Number.parseFloat(columns[44].trim());
-    currency.metrics[1].long.openInt = Number.parseFloat(columns[45].trim());
-    currency.metrics[1].short.openInt = Number.parseFloat(columns[46].trim());
-    currency.metrics[1].spreading.openInt = Number.parseFloat(columns[47].trim());
-    currency.metrics[2].long.openInt = Number.parseFloat(columns[48].trim());
-    currency.metrics[2].short.openInt = Number.parseFloat(columns[49].trim());
-    currency.metrics[2].spreading.openInt = Number.parseFloat(columns[50].trim());
+    currency.metrics[0].long.openInt = cleanNumber(columns[42]);
+    currency.metrics[0].short.openInt = cleanNumber(columns[43]);
+    currency.metrics[0].spreading.openInt = cleanNumber(columns[44]);
+    currency.metrics[1].long.openInt = cleanNumber(columns[45]);
+    currency.metrics[1].short.openInt = cleanNumber(columns[46]);
+    currency.metrics[1].spreading.openInt = cleanNumber(columns[47]);
+    currency.metrics[2].long.openInt = cleanNumber(columns[48]);
+    currency.metrics[2].short.openInt = cleanNumber(columns[49]);
+    currency.metrics[2].spreading.openInt = cleanNumber(columns[50]);
     /* Other reportables
     currency.metrics[3].short.changes = Number.parseFloat(columns[51].trim());
     currency.metrics[3].short.changes = Number.parseFloat(columns[52].trim());
@@ -160,16 +161,16 @@ const parseCurrency = (row) => {
      currency.metrics[3].short.changes = Number.parseFloat(columns[56].trim());
      currency.metrics[3].short.changes = Number.parseFloat(columns[57].trim());
      */
-    currency.totalTraders = Number.parseFloat(columns[58].trim());
-    currency.metrics[0].long.traders = Number.parseFloat(columns[59].trim());
-    currency.metrics[0].short.traders = Number.parseFloat(columns[60].trim());
-    currency.metrics[0].spreading.traders = Number.parseFloat(columns[61].trim());
-    currency.metrics[1].long.traders = Number.parseFloat(columns[62].trim());
-    currency.metrics[1].short.traders = Number.parseFloat(columns[63].trim());
-    currency.metrics[1].spreading.traders = Number.parseFloat(columns[64].trim());
-    currency.metrics[2].long.traders = Number.parseFloat(columns[65].trim());
-    currency.metrics[2].short.traders = Number.parseFloat(columns[66].trim());
-    currency.metrics[2].spreading.traders = Number.parseFloat(columns[67].trim());
+    currency.totalTraders = cleanNumber(columns[58]);
+    currency.metrics[0].long.traders = cleanNumber(columns[59]);
+    currency.metrics[0].short.traders = cleanNumber(columns[60]);
+    currency.metrics[0].spreading.traders = cleanNumber(columns[61]);
+    currency.metrics[1].long.traders = cleanNumber(columns[62]);
+    currency.metrics[1].short.traders = cleanNumber(columns[63]);
+    currency.metrics[1].spreading.traders = cleanNumber(columns[64]);
+    currency.metrics[2].long.traders = cleanNumber(columns[65]);
+    currency.metrics[2].short.traders = cleanNumber(columns[66]);
+    currency.metrics[2].spreading.traders = cleanNumber(columns[67]);
     /* Other reportables
     currency.metrics[3].short.changes = Number.parseFloat(columns[68].trim());
     currency.metrics[3].short.changes = Number.parseFloat(columns[69].trim());
@@ -197,6 +198,11 @@ const parseCurrency = (row) => {
      */
 
     return currency;
-}
+};
 
-module.exports = getCurrencies
+const cleanNumber = (string) => {
+    const value = string.trim();
+    return value === "." ? "." : Number.parseFloat(value);
+};
+
+module.exports = getCurrencies;
