5817afb5bd58d3223ee0978f7cff6041604df486
morkitet <morkitet@gmail.com>
2022-05-26 16:50:09 -0300

    added react-color-palette library from npm, added color picker

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,13 +1,16 @@
 import './App.css'
 import { ChartPositions } from './components/ChartPositions'
+import { ColorPicker, useColor } from "react-color-palette";
+import "react-color-palette/lib/css/styles.css";
 
 function App() {
-
+  const [color, setColor] = useColor("hex", "#121212");
   return (
     <div className="App" >
       <div className='grid'>
         <div>
-          <h1 className='gridChild'>test</h1>
+          <h1 className='gridChild'>Color Picker</h1>
+          <ColorPicker width={456} height={228} color={color} onChange={setColor} hideHSV dark />
         </div>
         <div className="chart">
           <ChartPositions />
