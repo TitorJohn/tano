e95eaa5e3089de1f76f4c5e7b018621aeff8dc44
morkitet <morkitet@gmail.com>
2022-05-26 16:29:15 -0300

    added json with data, resized chart to half the screen to fit a color picker on the other side

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -4,9 +4,16 @@ import { ChartPositions } from './components/ChartPositions'
 function App() {
 
   return (
-    <div className="App">
-      <ChartPositions />
-    </div >
+    <div className="App" >
+      <div className='grid'>
+        <div>
+          <h1 className='gridChild'>test</h1>
+        </div>
+        <div className="chart">
+          <ChartPositions />
+        </div>
+      </div>
+    </div>
   )
 }
 
